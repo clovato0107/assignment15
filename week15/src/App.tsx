7efeef3556ea83c9d3b6b8249c5c86517c98c444
clovato0107 <chrystallovato0107@gmail.com>
2024-10-18 18:54:20 -0700

    commit with 0 hard coded as id deleting two songs in a song list server isnt assigning an id to songs in a list

--- week15/src/App.tsx
+++ week15/src/App.tsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import SongList from './SongList';
-import SongForm from './SongForm';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import SongList from "./SongList";
+import SongForm from "./SongForm";
 
 export interface Song {
   id: number;
@@ -12,45 +12,81 @@ export interface Song {
 const App: React.FC = () => {
   const [songs, setSongs] = useState<Song[]>([]);
 
+  const URL = "http://localhost:3000/songs";
   // Fetch the songs from the JSON server
   useEffect(() => {
-    axios.get('http://localhost:5000/songs')
-      .then(response => setSongs(response.data))
-      .catch(error => console.error("Error fetching songs:", error));
+    // Code in here runs when the page loads.
+    getSongs();
   }, []);
+
+  const getSongs = async () => {
+    axios
+      .get(URL)
+      .then((response) => setSongs(response.data))
+      .catch((error) => console.error("Error fetching songs:", error));
+  };
 
   // Add a song
   const addSong = (newSong: Song) => {
-    axios.post('http://localhost:5000/songs', newSong)
-      .then(response => setSongs([...songs, response.data]))
-      .catch(error => console.error("Error adding song:", error));
+    axios
+      .post(URL, newSong)
+      .then((response) => setSongs([...songs, response.data]))
+      .catch((error) => console.error("Error adding song:", error));
   };
 
   // Update a song
+  //   const updateTask = (id: number, newText: string) => {
+  // fetch(`http://localhost:5000/tasks/${id}`, {
+  //   method: 'PUT',
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //   },
+  //   body: JSON.stringify({ text: newText }),
+  // })
+  //   .then(response => response.json())
+  //   .then(updatedTask => {
+  //     setTasks(tasks.map((task) => (task.id === id ? updatedTask : task)));
+  //   });
+
   const updateSong = (updatedSong: Song) => {
-    axios.put(`http://localhost:5000/songs/${updatedSong.id}`, updatedSong)
-      .then(() => {
-        setSongs(songs.map(song => song.id === updatedSong.id ? updatedSong : song));
+    axios
+      .put(`${URL}/${updatedSong.id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ updateSong }),
       })
-      .catch(error => console.error("Error updating song:", error));
+      .then((response) =>  response.data)
+      .then(() => {
+        setSongs(
+          songs.map((song) => (song.id === updatedSong.id ? updatedSong : song))
+        );
+      })
+      .catch((error) => console.error("Error updating song:", error));
   };
 
   // Delete a song
   const deleteSong = (id: number) => {
-    axios.delete(`http://localhost:5000/songs/${id}`)
-      .then(() => setSongs(songs.filter(song => song.id !== id)))
-      .catch(error => console.error("Error deleting song:", error));
+    axios
+      .delete(`${URL}/${id}`, {
+        method: "DELETE",
+      })
+      .then(() => setSongs(songs.filter((song) => song.id !== id)))
+      .catch((error) => console.error("Error deleting song:", error));
   };
 
   return (
-    <div className="App">
+    <div className="App container">
       <h1>My Playlist</h1>
       <SongForm onAddSong={addSong} />
-      <SongList songs={songs} onUpdateSong={updateSong} onDeleteSong={deleteSong} />
+      <SongList
+        songs={songs}
+        onUpdateSong={updateSong}
+        onDeleteSong={deleteSong}
+      />
     </div>
   );
 };
 
 export default App;
-
-
